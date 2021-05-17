7a1129c971a0f98ccc630b10c2488a0a3b054ff6
Khush <khushboosahubr.21@gmail.com>
2021-05-17 10:48:23 +0530

    Pet portal

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,6 @@
-// import React from 'react';
-// import { render, screen } from '@testing-library/react';
-// import App from './App';
-
-// test('renders learn react link', () => {
-//   render(<App />);
-//   const linkElement = screen.getByText(/learn react/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
 
 import { shallow, mount } from "enzyme";
 import Adapter from 'enzyme-adapter-react-16';
-// import Account from "./Account";
 import App from "./App";
 // import toJson from "enzyme-to-json";
 
