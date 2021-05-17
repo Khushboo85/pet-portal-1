5106c054394e366f4381c720562f78eede149c49
Khush <khushboosahubr.21@gmail.com>
2021-05-17 17:57:35 +0530

    docker build

--- src/components/Drawer/constants.ts
+++ src/components/Drawer/constants.ts
@@ -3,9 +3,11 @@ import { UserRole } from 'src/constants/roles';
 export enum DrawerOptions {
   Bookings = 'Bookings',
   Pets = 'Pets',
+  User ='User'
 }
 
 export const ROLE_TO_OPTIONS_MAP: any = {
   [UserRole.Employee]: [DrawerOptions.Bookings],
   [UserRole.User]: [DrawerOptions.Bookings, DrawerOptions.Pets],
+  [UserRole.Admin]: [DrawerOptions.Bookings, DrawerOptions.Pets, DrawerOptions.User]
 };
